ce7e18421d6f1ab5380667a87abfd851f259e764
Utkarsh Nadkarni <utkarsh.nadkarni@gmail.com>
2018-05-27 15:31:12 -0400

    Initial commit

---
+++ src/com/barrons/Main.java
@@ -0,0 +1,17 @@
+package com.barrons;
+
+import com.barrons.chapter1.ChapterOne;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChapterOne.problemTwo();
+	}
+
+}
---
+++ src/com/barrons/chapter1/ChapterOne.java
@@ -0,0 +1,13 @@
+package com.barrons.chapter1;
+
+public class ChapterOne {
+
+	public ChapterOne() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void problemTwo() {
+		System.out.println("\\* This is not\n a comment *\\");		
+	}
+
+}
